147ecf0f62e9a35a69e96de3603a147ee599aeea
Adam Kariv <adam.kariv@gmail.com>
2019-07-18 09:39:48 +0300

    +

--- dataflows_openspending/os_dgp_server.py
+++ dataflows_openspending/os_dgp_server.py
@@ -1,58 +1,11 @@
 import os
 
 from dgp_server.blueprint import DgpServer
+from dgp_server.config_storer import ConfigStorerDGP
 
-from dataflows import Flow, update_resource, \
-    set_primary_key, dump_to_sql
-from dgp.core import Context, Config, BaseDataGenusProcessor
-from dgp.config.consts import CONFIG_URL, CONFIG_TAXONOMY_ID
+from dgp.core import Context, Config
 
 from .publisher import PublisherDGP
-
-
-class ConfigStorerDGP(BaseDataGenusProcessor):
-
-    def __init__(self, config, context, lazy_engine):
-        super().__init__(config, context)
-        self.lazy_engine = lazy_engine
-        self.inner_publish_flow = lambda *_: None
-
-    def collate_values(self, fields):
-        def func(row):
-            return dict((f, row[f]) for f in fields)
-        return func
-
-    def flow(self):
-        TARGET = 'configurations'
-        saved_config = self.config._unflatten()
-        saved_config.setdefault('publish', {})['allowed'] = False
-
-        return Flow(
-            [
-                dict(
-                    source=self.config.get(CONFIG_URL),
-                    snippets=[
-                        '{}: {}'.format(
-                            self.config.get(CONFIG_TAXONOMY_ID),
-                            os.path.basename(self.config.get(CONFIG_URL))
-                        )
-                    ],
-                    config=saved_config,
-
-                )
-            ],
-            update_resource(-1, name=TARGET),
-            set_primary_key(['source']),
-            dump_to_sql(
-                dict([
-                    (TARGET, {
-                        'resource-name': TARGET,
-                        'mode': 'update'
-                    })
-                ]),
-                engine=self.lazy_engine(),
-            ),
-        )
 
 
 class OsDgpServer(DgpServer):
--- dataflows_openspending/server.py
+++ dataflows_openspending/server.py
@@ -1,32 +1,11 @@
-import os
 import logging
 
 from aiohttp import web
-import aiopg.sa
 
 from .os_dgp_server import OsDgpServer
-from .configurations import configs
 
 app = web.Application()
 app.add_subapp('/api', OsDgpServer())
-app.router.add_get('/configs', configs)
-
-
-async def init_pg(app):
-    if 'DATABASE_URL' in os.environ:
-        engine = await aiopg.sa.create_engine(os.environ['DATABASE_URL'])
-    else:
-        engine = None
-    app['db'] = engine
-
-
-async def close_pg(app):
-    app['db'].close()
-    await app['db'].wait_closed()
-
-
-app.on_startup.append(init_pg)
-app.on_cleanup.append(close_pg)
 
 logging.getLogger().setLevel(logging.INFO)
 
